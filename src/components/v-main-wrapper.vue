39ced420458883b1cd39ad59e63ebc917504c90e
arhipovivn <arhipovivn@gmail.com>
2023-03-09 10:21:22 +0300

    added select menu

--- src/components/v-main-wrapper.vue
+++ src/components/v-main-wrapper.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="v-main-wrapper">
-   <h1>
-   </h1> 
+<v-select
+:options="options"
+:selected="selected"
+@select="optionSelect"  
+/>
+<p> sel:
+{{ selected.name }}
+</p>
    <!-- в этой области показываются компоненты в зависимости от адреса т.е либо карт или каталог  -->
    <!-- <keep-alive> сохранение состояния при изменении -->
 
@@ -21,20 +27,36 @@
 // import VCatalog from './v-catalog'
 // import VCart from './v-cart.vue';
 import{mapGetters} from 'vuex';//чтобы поймать геттер корзины
+import vSelect from './v-select.vue';
 export default {
     name:"v-main-wrapper",
   components: { 
+    vSelect
     //VCatalog, 
     // VCart
    },
     data(){
        return{
-        title :'CATALOG'
+        title :'CATALOG',
+        options :[
+          {name:'Options 1',value: 1},
+          {name:'Options 2',value: 2},
+          {name:'Options 3',value: 3},
+          {name:'Options 4',value: 4},
+          {name:'Options 5',value: 5}
+        ],
+        selected:'Select'
        } 
     }, 
      computed :{
       ...mapGetters(['CART'])// сюда положили товар 
 
+    },
+    methods:{
+      optionSelect(option){
+        this.selected = option
+
+      }
     }
 }
 </script>
---
+++ src/components/v-select.vue
@@ -0,0 +1,72 @@
+<template>
+<div class="v-select"
+>
+<!-- сворачивавание меню -->
+<p class="title"
+@click="areOptionsVis = !areOptionsVis" 
+>{{selected.name}}</p>
+<div class="options"
+v-if="areOptionsVis"
+>
+
+<p
+v-for="option in options"
+:key="option.value"
+@click="selectOption(option)"
+>
+{{ option.name }}
+</p></div>
+</div>
+</template>
+<script>
+export default {
+    data() {
+return{
+    areOptionsVis: false // сворачивавание меню
+}
+},
+    name: "v-select",
+    props: { // добавляю пропсы после их связываю в родителе с родиельскими элементами в атрибутах этого компонента 
+options:{
+    type:Array,
+    default(){
+        return []
+    }
+},
+selected:{
+    type: String,
+    default: ''
+}
+
+    },
+    methods:{
+        selectOption(option){
+this.$emit('select',option) // передаю родителю эту функцию с переменной option
+this.areOptionsVis=false // закрытие селекта когда на него нажимаю 
+        }
+    }
+}
+</script>
+<style>
+.title{
+    cursor: pointer;
+    border: solid 1px green;
+}
+.v-select{
+    position: relative;
+    width: 200px;
+}
+.v-select p{
+ margin: 0;
+}
+.options{
+   border: solid 1px green; 
+   position: absolute;
+   top: 20px;
+   right: 0;
+   width: 100%;
+}
+.options p:hover{
+    background-color: green;
+}
+</style>
